ddd52843bc3128a5216eb8f12fef8532fb16f022
Stijnb1312 <1330990@student.rocvantwente.nl>
2025-09-18 14:16:09 +0200

    Werkwijze duidelijk gemaakt

---
+++ Groep_9/Groep_9/Controllers/Blok3Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Groep_9.Controllers
+{
+    public class Blok3Controller : Controller
+    {
+        public IActionResult Opdracht1()
+        {
+
+        string test = "Hoi";
+            return View("Opdracht1", test);
+
+            //code van opdracht 1 hier
+
+        }
+
+        public IActionResult Opdracht2()
+        {
+            return View();
+
+            //code van opdracht 2 hier
+        }
+    }
+}
