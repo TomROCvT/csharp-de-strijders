269bb283c6807c3436927dc67d4755d61b1d46ca
TomROCvT <1345377@student.rocvantwente.nl>
2025-09-23 12:07:37 +0200

    Opdracht 3_3 en 3_4 gemaakt

--- Groep_9/Groep_9/Controllers/Blok3Controller.cs
+++ Groep_9/Groep_9/Controllers/Blok3Controller.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Groep_9.Controllers
@@ -8,7 +9,7 @@ namespace Groep_9.Controllers
         public IActionResult Opdracht1()
         {
 
-        string test = "Hoi";
+            string test = "Hoi";
             return View("Opdracht1", test);
 
             //code van opdracht 1 hier
@@ -20,6 +21,37 @@ namespace Groep_9.Controllers
             return View();
 
             //code van opdracht 2 hier
+        }
+
+        public IActionResult Opdracht3(double hoogte, double lengte, double breedte)
+        {
+            double inhoud = hoogte * lengte * breedte;
+            ViewBag.Inhoud = inhoud.ToString("0.00");
+            return View();
+        }
+
+        public IActionResult Opdracht4(double euroBedrag)
+        {
+            // Actuele koersen van EUR naar andere valuta’s (op moment van schrijven)
+            double eurToUsd = 1.1781;
+            double eurToGbp = 0.87290;
+            double eurToJpy = 174.17;
+            double eurToChf = 0.9348;
+            double eurToAud = 1.7865;
+
+            var resultaten = new Dictionary<string, string>();
+
+            // Berekeningen
+            resultaten["USD"] = (euroBedrag * eurToUsd).ToString("0.00", CultureInfo.InvariantCulture);
+            resultaten["GBP"] = (euroBedrag * eurToGbp).ToString("0.00", CultureInfo.InvariantCulture);
+            resultaten["JPY"] = (euroBedrag * eurToJpy).ToString("0.00", CultureInfo.InvariantCulture);
+            resultaten["CHF"] = (euroBedrag * eurToChf).ToString("0.00", CultureInfo.InvariantCulture);
+            resultaten["AUD"] = (euroBedrag * eurToAud).ToString("0.00", CultureInfo.InvariantCulture);
+
+            ViewBag.Resultaten = resultaten;
+            ViewBag.EuroBedrag = euroBedrag.ToString("0.00", CultureInfo.InvariantCulture);
+
+            return View();
         }
     }
 }
